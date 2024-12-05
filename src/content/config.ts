1c549fcc314dcc09a6edd9e2ba545f0ec5a4fdc5
Marko Anastasov <marko@operately.com>
2024-12-05 18:39:00 +0100

    responsive
    Signed-off-by: Marko Anastasov <marko@operately.com>

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,14 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-  type: 'content',
+  type: "content",
   schema: z.object({
     title: z.string(),
     description: z.string().optional(),
-    pubDate: z.string()
-      .transform((str) => new Date(str)),
-    type: z.enum(['article', 'link', 'photo']),
+    pubDate: z.string().transform((str) => new Date(str)),
+    type: z.enum(["article", "link", "photo"]),
     url: z.string().optional(), // for link type posts
-    image: z.string().optional(), // for photo type posts
+    image: z.string().optional(), // for photo2 type posts
     draft: z.boolean().default(false),
   }),
 });
